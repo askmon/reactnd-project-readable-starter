84c0148e9fdc57bd5a1a9622b0dfda05576a0bf2
André <andre.spanguero@gmail.com>
2018-01-02 18:37:06 -0200

    Add post reducer

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,5 +1,5 @@
 export const GET_CATEGORIES = 'GET_CATEGORIES';
-export const GET_POST = '';
+export const GET_POST = 'GET_POST';
 export const GET_POSTS = 'GET_POSTS';
 export const GET_POSTS_CATEGORY = 'GET_POSTS_CATEGORY';
 export const CREATE_POST = 'CREATE_POST';
---
+++ frontend/src/reducers/post.js
@@ -0,0 +1,82 @@
+import {
+  GET_POST,
+  GET_POSTS,
+  GET_POSTS_CATEGORY,
+  CREATE_POST,
+  UPDATE_VOTE,
+  EDIT_POST,
+  DELETE_POST
+} from '../actions'
+
+const initialState = {
+  posts: [],
+  categoryPosts: [],
+  post: {}
+}
+
+function postReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_POST:
+    const { post } = action
+    return {
+      ...state,
+      post: post
+    }
+    case GET_POSTS:
+      const { initialPosts } = action
+      return {
+        ...state,
+        posts: initialPosts
+      }
+    case GET_CATEGORY_POSTS:
+      const { categoryPosts } = action
+      return {
+        ...state,
+        categoryPosts: categoryPosts
+      }
+    case CREATE_POST:
+      const { newPost } = action
+      return {
+        ...state,
+        categoryPosts: state.categoryPosts.concat(newPost)
+      }
+    case EDIT_POST:
+      const { editedPost } = action
+      return {
+        ...state,
+        posts: state.posts
+          .filter(p => p.id !== editedPost.id)
+          .push(editedPost),
+        categoryPosts: state.categoryPosts
+          .filter(p => p.id !== editedPost.id)
+          .push(editedPost),
+        post: editedPost
+      }
+    case DELETE_POST:
+      const { deletedId } = action
+      return {
+        ...state,
+        posts: state.posts.filter(p => p.id !== deletedId),
+        categoryPosts: state.categoryPosts.filter(p => p.id !== deletedId),
+        post: state.post.id === deletedId ? {} : state.post
+      }
+    case UPDATE_POST_SCORE:
+      const { postWithNewScore } = action
+      const updatedPosts = state.posts.map(p => {
+        return p =  p.id === postWithNewScore.id ? postWithNewScore : p
+      })
+      const updatedCategoryPosts = state.categoryPosts.map(p => {
+        return p =  p.id === postWithNewScore.id ? postWithNewScore : p
+      })
+      return {
+        ...state,
+        posts: updatedPosts,
+        post: postWithNewScore,
+        categoryPosts: updatedCategoryPosts
+      }
+    default :
+      return state
+  }
+}
+
+export default postReducer;
